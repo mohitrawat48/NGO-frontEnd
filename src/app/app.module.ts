462b3d1df1d1c95450ba466d5a8e16c0175dc422
Mohit Rawat <mohitrawat48@gmail.com>
2019-07-18 10:03:43 -0400

    adminpage and user page removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,33 @@ import { AuthGuard } from "./auth.guard";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { UserpageComponent } from "./userpage/userpage.component";
-import { AdminpageComponent } from "./adminpage/adminpage.component";
 import { LoginComponent } from "./login/login.component";
-import { AdminUserComponent } from './admin-user/admin-user.component';
-import { NonAdminUserComponent } from './non-admin-user/non-admin-user.component';
-import { RegisterComponent } from './register/register.component';
-import { DonationMgmtComponent } from './donation-mgmt/donation-mgmt.component';
-import { UserMgmtComponent } from './user-mgmt/user-mgmt.component';
-import { UserViewComponent } from './user-view/user-view.component';
-import { UserCartComponent } from './user-cart/user-cart.component';
-import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+import { AdminUserComponent } from "./admin-user/admin-user.component";
+import { NonAdminUserComponent } from "./non-admin-user/non-admin-user.component";
+import { RegisterComponent } from "./register/register.component";
+import { DonationMgmtComponent } from "./donation-mgmt/donation-mgmt.component";
+import { UserMgmtComponent } from "./user-mgmt/user-mgmt.component";
+import { UserViewComponent } from "./user-view/user-view.component";
+import { UserCartComponent } from "./user-cart/user-cart.component";
+import { OrderConfirmComponent } from "./order-confirm/order-confirm.component";
 
 export function tokenGetter() {
   return localStorage.getItem("access_token");
 }
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, AdminUserComponent, NonAdminUserComponent, RegisterComponent, DonationMgmtComponent, UserMgmtComponent, UserViewComponent, UserCartComponent, OrderConfirmComponent],
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    AdminUserComponent,
+    NonAdminUserComponent,
+    RegisterComponent,
+    DonationMgmtComponent,
+    UserMgmtComponent,
+    UserViewComponent,
+    UserCartComponent,
+    OrderConfirmComponent
+  ],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
