ce3df401cab04a2cf49e9bea9428c2d19225d02c
Kenny Quach <kq22@scarletmail.rutgers.edu>
2019-07-18 16:05:23 -0400

    Register Component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  email = new FormControl('');
+  passwords = new FormControl("");
+  firstName = new FormControl('');
+  lastName = new FormControl('');
+  middleName = new FormControl("");
+
+
+  updateEmail() {
+    this.email.setValue('ankit@appdividend.com');
+  }
+  
+  updateFirstName() {
+    this.firstName.setValue('ankit');
+  }
+  updateLastName() {
+    this.lastName.setValue('John');
+  }
+
+  setPassword() {
+    this.passwords.setValue('John');
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onsubmit(){
+
+    
+
   }
 
 }
