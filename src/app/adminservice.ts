dc77c3e74b172d1b51385354a3987e5abf61bee1
Mohit Rawat <mohitrawat48@gmail.com>
2019-07-22 10:39:05 -0400

    some mionr changes

--- src/app/adminservice.ts
+++ src/app/adminservice.ts
@@ -1,33 +1,31 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { Admin } from 'src/app/admin';
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
-
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Admin } from "src/app/admin";
+import { Observable, throwError } from "rxjs";
+import { catchError } from "rxjs/operators";
 
 @Injectable({
-    providedIn: 'root'
-  })
-
+  providedIn: "root"
+})
 export class AdminService {
-    private _url: string = 'http://localhost:3000/api/user';
-    constructor(private http: HttpClient) { }
-    getdetail(): Observable<Admin[]> {
-        return this.http.get<Admin[]>(this._url)
-        .pipe(catchError(this.errorHandler));
-    }
-    postUser(user: Admin): Observable<Admin> {
-        return this.http.post<Admin>(this._url, {
-          firstName: user.FirstName,
-          lastName: user.LastName,
-          email: user.Email,
-          password: user.Password,
-          adminRole: user.Role
-        })
-        .pipe(catchError(this.errorHandler));
-      }
+  private _url: string = "http://localhost:3000/api/user";
+  constructor(private http: HttpClient) {}
+  getdetail(): Observable<Admin[]> {
+    return this.http.get<Admin[]>(this._url).pipe(catchError(this.errorHandler));
+  }
+  postUser(user: Admin): Observable<Admin> {
+    return this.http
+      .post<Admin>(this._url, {
+        firstName: user.FirstName,
+        lastName: user.LastName,
+        email: user.Email,
+        password: user.Password,
+        adminRole: user.Role
+      })
+      .pipe(catchError(this.errorHandler));
+  }
 
-  errorHandler(error: HttpErrorResponse){
+  errorHandler(error: HttpErrorResponse) {
     return throwError(error.message || "Server Error");
   }
 }
--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,30 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import {FormGroup, FormControl, FormBuilder} from '@angular/forms';
-import { ActivatedRoute, Router, ParamMap } from '@angular/router';
-import { Admin } from './../admin';
-import { AdminService } from './../adminservice';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl, FormBuilder } from "@angular/forms";
+import { ActivatedRoute, Router, ParamMap } from "@angular/router";
+import { Admin } from "./../admin";
+import { AdminService } from "./../adminservice";
 @Component({
-  selector: 'app-create',
-  templateUrl: './create.component.html',
-  styleUrls: ['./create.component.css'],
-
+  selector: "app-create",
+  templateUrl: "./create.component.html",
+  styleUrls: ["./create.component.css"]
 })
 export class CreateComponent implements OnInit {
-  public newevent:any;
+  public newevent: any;
   constructor(private _Service: AdminService, private fb: FormBuilder) {
     this.newevent = new FormGroup({
       FirstName: new FormControl(""),
       LastName: new FormControl(""),
-      Email : new FormControl(""),
+      Email: new FormControl(""),
       Role: new FormControl("")
-
     });
-   }
-
-  ngOnInit() {
   }
 
-  onSubmit(){
+  ngOnInit() {}
+
+  onSubmit() {
     const result = this.newevent.value as Admin;
     this._Service.postUser(result).subscribe(res => {
       // response from REST API call
